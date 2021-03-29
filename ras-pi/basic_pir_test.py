18f9c8465b3a2cbcc8b42f3d28ea9ef0edde5ccc
AndrewCouncil <andrew.p.council@gmail.com>
2021-03-28 20:48:13 -0400

    setting up to use as actual motion sensor

--- ras-pi/basic_pir_test.py
+++ ras-pi/basic_pir_test.py
@@ -1,7 +1,18 @@
-from gpiozero import MotionSensor
-from time import sleep
+import RPi.GPIO as GPIO
+import time
 
-pir = MotionSensor(24)
-while True:
-    print(pir.motion_detected)
-    sleep(.5)
+GPIO.setmode(GPIO.BCM)
+PIR_PIN = 24
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+try:
+               print “PIR Module Test (CTRL+C to exit)”
+               time.sleep(2)
+               print “Ready”
+               while True:
+                             if GPIO.input(PIR_PIN):
+                                             print “Motion Detected!”
+                              time.sleep(1)
+except KeyboardInterrupt:
+               print “ Quit”
+               GPIO.cleanup()
