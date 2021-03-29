ebb762acaa784e4484e60ab427613b88fb6ecfb1
AndrewCouncil <andrew.p.council@gmail.com>
2021-03-28 20:41:38 -0400

    setting up to use as actual motion sensor

--- ras-pi/basic_pir_test.py
+++ ras-pi/basic_pir_test.py
@@ -1,5 +1,7 @@
 from gpiozero import MotionSensor
+from time import sleep
 
 pir = MotionSensor(18)
-pir.wait_for_motion()
-print("Motion detected!")
+while True:
+    print(pir.motion_detected)
+    sleep(.5)
