bb00922504c58f360bf092efa36375e498993248
AndrewCouncil <andrew.p.council@gmail.com>
2021-04-12 12:55:30 -0400

    making boot script

--- ras-pi/rads_boot.py
+++ ras-pi/rads_boot.py
@@ -2,6 +2,12 @@ from gpiozero import RGBLED
 from colorzero import Color
 import requests, os, sys
 
+try:
+    os.system("git pull /home/pi/RADS-code")
+except:
+    raise RuntimeError("Pulling of updated program failed!")
+
+
 print("Starting rgb")
 os.system("sudo python rgb_test.py &")
 print("started already")
